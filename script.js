9f61b116208fab5a5b69262cb91ce3544f3fa726
Luqman abbas <116719637+Usamakilo@users.noreply.github.com>
2023-12-05 12:48:57 +0000

    added timer and more features

--- script.js
+++ script.js
@@ -1,36 +1,101 @@
-var question = "";
-var solution = -1;
+let question = "";
+let solution = -1;
+let score = 0;
+let timerInterval;
+const inputForm = document.getElementById("inputForm");
+const input = document.getElementById("input");
+const newGameButton = document.getElementById("newGameButton");
+const scoreElement = document.getElementById("score");
+const secondsElement = document.getElementById("seconds");
+const questionImage = document.getElementById("question");
+const resultMessageElement = document.getElementById("result-message");
+const loadingSpinner = document.getElementById("loading-spinner");
 
 const newGame = () => {
   initGame();
+  resultMessageElement.innerText = "";
 };
 
-const handleInput = (x) => {
-  let inp = document.getElementById("input");
-  if (inp.value == solution) {
-    console.log("Correct!");
-    newGame();
+const onSubmit = (e) => {
+  e.preventDefault();
+  clearInterval(timerInterval);
+
+  if (input.value == solution) {
+    score++;
+    resultMessageElement.innerText = "Correct!";
   } else {
-    console.log("Incorrect!");
+    score--;
+    resultMessageElement.innerText = "Incorrect!";
   }
+
+  updateScore();
+  displayResult();
 };
 
 const startGame = (data) => {
-  question = data.question;
-  solution = data.solution;
-  let img = document.getElementById("question");
-  img.src = question;
+  newGameButton.style.display = "none";
+  inputForm.style.display = "block";
+  inputForm.reset();
+  ({ question, solution } = data);
+  loadingSpinner.classList.add("hidden");
+  questionImage.src = question;
+  countdownTimer();
+};
+
+const showLoading = () => {
+  loadingSpinner.classList.remove("hidden");
 };
 
 const fetchQuestion = async () => {
-  let response = await fetch("https://marcconrad.com/uob/tomato/api.php");
-  let data = await response.json();
-  console.log(data);
-  startGame(data);
+  try {
+    showLoading();
+    const response = await fetch("https://marcconrad.com/uob/tomato/api.php");
+    const data = await response.json();
+    startGame(data);
+  } catch (error) {
+    console.error("Error fetching question:", error);
+    loadingSpinner.classList.add("hidden");
+  }
 };
 
 const initGame = () => {
   fetchQuestion();
+  resultMessageElement.innerText = "";
+};
+
+const countdownTimer = () => {
+  let seconds = 30;
+
+  timerInterval = setInterval(() => {
+    secondsElement.innerText = seconds;
+
+    if (seconds === 0) {
+      clearInterval(timerInterval);
+      handleTimeout();
+    } else {
+      seconds--;
+    }
+  }, 1000);
+};
+
+const handleTimeout = () => {
+  resultMessageElement.innerText =
+    "Time's up! The correct answer is " + solution;
+  score--;
+  inputForm.style.display = "none";
+  newGameButton.style.display = "block";
+  updateScore();
+};
+
+const updateScore = () => {
+  scoreElement.innerText = score;
+};
+
+const displayResult = () => {
+  inputForm.style.display = "none";
+  newGameButton.style.display = "block";
 };
 
 initGame();
+inputForm.addEventListener("submit", onSubmit);
+newGameButton.addEventListener("click", newGame);
